7e670c7323d524d559a944125c0e702b6eb28047
OneGreekGuy <k_ostakis@hotmail.com>
2021-06-26 20:21:03 +0300

    Update adaptive_active_learning.py

--- libact/query_strategies/multilabel/adaptive_active_learning.py
+++ libact/query_strategies/multilabel/adaptive_active_learning.py
@@ -150,8 +150,7 @@ class AdaptiveActiveLearning(QueryStrategy):
 
         #    approx_err.append(np.sum(err))
 
-        choices = np.where(np.array(approx_err) == np.min(approx_err))[0]
-        print(choices)
+        choices = np.where(np.array(approx_err) == np.min(approx_err))[0]        
         ask_idx = candidates[self.random_state_.choice(choices)]
 
-        return unlabeled_entry_ids[ask_idx],choices
+        return (unlabeled_entry_ids[ask_idx],choices)
