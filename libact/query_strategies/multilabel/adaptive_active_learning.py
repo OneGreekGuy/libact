5e8077c45de946547edfe83a2d52a7da66c1e6d1
OneGreekGuy <k_ostakis@hotmail.com>
2021-06-26 20:14:18 +0300

    Update adaptive_active_learning.py

--- libact/query_strategies/multilabel/adaptive_active_learning.py
+++ libact/query_strategies/multilabel/adaptive_active_learning.py
@@ -154,4 +154,4 @@ class AdaptiveActiveLearning(QueryStrategy):
         print(choices)
         ask_idx = candidates[self.random_state_.choice(choices)]
 
-        return unlabeled_entry_ids[ask_idx]
+        return unlabeled_entry_ids[ask_idx],choices
